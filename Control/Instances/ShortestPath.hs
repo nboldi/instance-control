503d9b98dd0695491f55b805d68f0b4c5f75d3ed
nboldi <nboldi@elte.hu>
2017-09-27 08:32:47 +0200

    Updated for the newest GHC version

--- Control/Instances/ShortestPath.hs
+++ Control/Instances/ShortestPath.hs
@@ -3,16 +3,11 @@
 module Control.Instances.ShortestPath where
 
 import Control.Monad.Identity
-import Control.Monad.Trans.Maybe
-import Control.Monad.Trans.List
-import Control.Monad.State
-import Data.Maybe
 import Data.Proxy
-import GHC.TypeLits
 import Control.Instances.TypeLevelPrelude
 
 -- * Generic datatypes to store connections
-  
+
 data Connect m1 m2
 data Connect_2m m1 m2
 data Connect_mt mt
@@ -22,21 +17,21 @@ data Connect_MU m
 -- | Marks that there is no legal path between the two types according
 -- to the rulebase.
 data NoPathFound
-  
+
 type family ShortestPath (e :: [*]) (s :: * -> *) (t :: * -> *) :: [*] where
   ShortestPath e t t = '[]
   ShortestPath e Identity t = '[ Connect_id t ]
   ShortestPath e s Proxy = '[ Connect_MU s ]
   ShortestPath e s t = ShortestPath' e s (InitCurrent e t)
-  
+
 type family ShortestPath' (e :: [*]) (s :: * -> *) (c :: [[*]]) :: [*] where
   ShortestPath' e s '[] = '[ NoPathFound ]
   ShortestPath' e s c = FromMaybe (ShortestPath' e s (ApplyEdges e c c))
-                                  (GetFinished s c) 
-                                  
-                                      
+                                  (GetFinished s c)
+
+
 type family GetFinished s c where
-  GetFinished s ((Connect s b ': p) ': lls) 
+  GetFinished s ((Connect s b ': p) ': lls)
     = Just (Connect s b ': p)
   GetFinished s (p ': lls) = GetFinished s lls
   GetFinished s '[] = Nothing
@@ -44,17 +39,17 @@ type family GetFinished s c where
 type family InitCurrent (e :: [*]) (t :: * -> *) :: [[*]] where
   InitCurrent '[] t = '[]
   InitCurrent (e ': es) t = IfJust (ApplyEdge e t)
-                                   ('[ MonomorphEnd e t ] ': InitCurrent es t) 
+                                   ('[ MonomorphEnd e t ] ': InitCurrent es t)
                                    (InitCurrent es t)
-  
+
 type family ApplyEdges (e :: [*]) (co :: [[*]]) (c :: [[*]]) :: [[*]] where
-  ApplyEdges (e ': es) co ((Connect s b ': p) ': cs) 
-    = AppendJust (IfThenJust (IsJust (ApplyEdge e s)) 
-                                (MonomorphEnd e s ': Connect s b ': p)) 
+  ApplyEdges (e ': es) co ((Connect s b ': p) ': cs)
+    = AppendJust (IfThenJust (IsJust (ApplyEdge e s))
+                                (MonomorphEnd e s ': Connect s b ': p))
                  (ApplyEdges (e ': es) co cs)
   ApplyEdges (e ': es) co '[] = ApplyEdges es co co
-  ApplyEdges '[] co cr = '[]  
-  
+  ApplyEdges '[] co cr = '[]
+
 type family ApplyEdge e t :: Maybe (* -> *) where
   ApplyEdge (Connect ms mr) mr = Just ms
   ApplyEdge (Connect_2m ms mt) (mt m) = Just ms
@@ -65,5 +60,3 @@ type family MonomorphEnd c v :: * where
   MonomorphEnd (Connect_2m m m') v = Connect m v
   MonomorphEnd (Connect_mt t) (t m) = Connect m (t m)
   MonomorphEnd c v = c
-
-
